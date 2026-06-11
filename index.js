8fa37689ac2f4b314bf9d935b76737fa7487358f
Karan Harsh Singh <ask.geminiservices@gmail.com>
2026-06-11 15:45:51 +0530

    Initial backend

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("QuickTools Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
